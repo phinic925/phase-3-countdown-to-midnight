5cb127b1669a314a6015dbfb41fffb0560d9440a
phinic925 <phinicnyangate480@gmail.com>
2022-08-18 17:33:56 +0100

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown integer
+    while integer >= 1
+        puts "#{integer} SECOND(S)!"
+      sleep 1
+      integer -=1
+   end
+   return 'HAPPY NEW YEAR!'
+        
+    end
+
 end
